7eebeb7ae38b83919117a551ca60190e21b5ec25
ErhanBallieker <erhanballieker@hotmail.com>
2016-05-21 00:53:33 +0300

    Revert "123"
    This reverts commit fdc670072689ed4b83eedf88a53e78b3356969a7.

--- Reos.XForms.App/Reos.XForms.App/App.xaml.cs
+++ Reos.XForms.App/Reos.XForms.App/App.xaml.cs
@@ -7,7 +7,7 @@ namespace Reos.XForms.App
 {
     public partial class App : Application, ILoginHelper
     {
-        //test check.. edit...234...
+        //test check.. edit...12341233455
         public App()
         {
             InitializeComponent();
